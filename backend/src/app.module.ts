696744ba3d6fb93fe489dc4a2723c4232056cefc
HernandoTW <97920818+HernandoTW@users.noreply.github.com>
2024-09-20 19:14:10 -0500

    TypeOrm config.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,3 +8,22 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '010203',
+      database: 'wompi_shop',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+})
+
